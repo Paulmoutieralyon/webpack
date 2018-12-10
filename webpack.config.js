0fdae770efb0fff1c5e5170912028e0b5b5cf6b4
Paul Moutier <moutier.paul@gmail.com>
2018-12-10 11:03:01 +0100

    webpack v1.0

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    entry: {
+        app: './js/index.js'
+    },
+
+    output: {
+        filename: 'mybundle.js'
+    }
+}
+
+const path = require('path');
+
+module.exports = {  
+    entry: {  
+        app: './js/index.js'  
+  },  
+
+    output: {  
+        path: path.resolve(__dirname, 'dist'),  
+        filename: 'bundle.js'  
+  }  
+};
